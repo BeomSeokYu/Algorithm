7aae5cf84f29a912252e7c59bdfec1d0c88118b0
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-02-20 21:34:31 +0900

    구현

---
+++ CodingTest/구현/시각.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+cnt = 0
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in list(str(h)+str(m)+str(s)):
+                cnt += 1
+print(cnt)
