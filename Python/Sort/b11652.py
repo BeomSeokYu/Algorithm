3b573aa37e91c04ae5bf979590eaa2c8e48c05dc
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-02-26 16:28:00 +0900

    b11652:  add comment

--- Python/Sort/b11652.py
+++ Python/Sort/b11652.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-cntDic = {}
+cntDic = {}                 # {숫자 : 갯수} 딕셔너리
 n = int(input())
 for i in range(n):
     num = int(input())
-    if num not in cntDic:
-        cntDic[num] = 1
-    else:
-        cntDic[num] += 1
-result = sorted(cntDic.items(), key=lambda x: x[0])
-result = sorted(result, key=lambda x: x[1], reverse=True)
-print(result[0][0])
+    if num not in cntDic:   # 아직 키가 아니면
+        cntDic[num] = 1     # 1
+    else:                   # 이미 키가 있으면
+        cntDic[num] += 1    # +1
+result = sorted(cntDic.items(), key=lambda x: x[0])         # 먼저 키로 정렬
+result = sorted(result, key=lambda x: x[1], reverse=True)   # 값 내림차순으로 정렬
+print(result[0][0])                                         # 가장 앞의 값 출력
