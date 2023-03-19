564273353e0dbb207bdeee2f8e3aca6d46fc3959
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-03-19 15:18:36 +0900

    개미전사 Modify

--- CodingTest/DP/개미전사.py
+++ CodingTest/DP/개미전사.py
@@ -9,7 +9,7 @@ input = sys.stdin.readline
 n = int(input())
 k = list(map(int, input().split()))
 
-d = [0] * (n + 1)
+d = [0] * n
 d[0], d[1] = k[0], max(k[0],k[1])
 for i in range(2, n):
     d[i] = max(d[i - 1], d[i - 2] + k[i])
