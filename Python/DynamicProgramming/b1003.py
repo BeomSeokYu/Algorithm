2bc5cadc64149ec89f29973ed34cc46342a0cd22
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-03-22 20:22:30 +0900

    b1003

---
+++ Python/DynamicProgramming/b1003.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+for i in range(int(input())):
+    n = int(input())
+
+    zeros=[1,0,1]
+    ones=[0,1,1]
+    if n >= 3:
+        for i in range(2, n):
+            zeros.append(zeros[i-1] + zeros[i])
+            ones.append(ones[i-1] + ones[i])
+    print(zeros[n], ones[n])
