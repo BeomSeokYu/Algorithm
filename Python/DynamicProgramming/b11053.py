cea812b74cd1c987491ca4fed3358172d07c2460
BeomSeok Yu <69231700+BeomSeokYu@users.noreply.github.com>
2023-03-23 20:29:51 +0900

    b11053 add comment

--- Python/DynamicProgramming/b11053.py
+++ Python/DynamicProgramming/b11053.py
@@ -1,14 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-
 n = int(input())
 arr = list(map(int, input().split()))
-d = [1] * (n + 1)
+d = [1] * (n + 1)                       # 인덱스 맞춤
 
-for i in range(n):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            d[i] = max(d[i], d[j] + 1)
-_max = max(d)
+for i in range(n):                      # i가 선택한 값이면
+    for j in range(i):                  # i 이전 값 중에
+        if arr[i] > arr[j]:             # i 보다 작은거 있으면
+            d[i] = max(d[i], d[j] + 1)  # dp테이블 값에 +1 한거랑 비교해 큰 값 저장
+_max = max(d)                           # 가장 긴 값을 선택
 print(_max)
