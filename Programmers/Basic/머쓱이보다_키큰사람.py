fb40d14c15eb6d6617674c09fd4d30bb811cf527
BeomSeok Yu <beomseok.dev@gmail.com>
2023-05-24 10:02:28 +0900

    basic - array

---
+++ Programmers/Basic/머쓱이보다_키큰사람.py
@@ -0,0 +1,9 @@
+def solution(array, height):
+    answer = 0
+    array.sort(reverse=True)
+    for i in range(len(array)):
+        if array[i] > height:
+            answer += 1
+        else:
+            break
+    return answer
