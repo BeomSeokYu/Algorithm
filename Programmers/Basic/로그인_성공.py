47b5db518f101e49fe49ce6be6499e29c20cdfa1
BeomSeok Yu <beomseok.dev@gmail.com>
2023-06-24 14:44:57 +0900

    basic - map

---
+++ Programmers/Basic/로그인_성공.py
@@ -0,0 +1,18 @@
+def solution(id_pw, db):
+    answer = ''
+    id_db = []
+    pw_db = []
+    vaildId = False
+    vaildPw = False
+    for i, p in db:
+        id_db.append(i)
+        pw_db.append(p)
+
+    if id_pw in db:
+        answer = 'login'
+    elif id_pw[0] not in id_db:
+        answer = 'fail'
+    else:
+        answer = 'wrong pw'
+
+    return answer
