41ce380f1e46f6cf05fb7f763b2a5354fbb3914f
BeomSeok Yu <beomseok.dev@gmail.com>
2023-08-06 09:18:54 +0900

    b11279 - max heap

---
+++ Baekjoon/Python/DataStructure/b11279.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+q = []
+N = int(input())
+for _ in range(N):
+    num = int(input())
+    if num != 0:
+        heapq.heappush(q, -num)
+    else:
+        if len(q) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(q))
