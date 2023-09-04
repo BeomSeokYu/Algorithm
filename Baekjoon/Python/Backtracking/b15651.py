17658d9a008146f1e1845845c65885131d4cfd7f
BeomSeok Yu <beomseok.dev@gmail.com>
2023-09-04 22:42:01 +0900

    b15651 - 백트래킹

---
+++ Baekjoon/Python/Backtracking/b15651.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+result = [1] * M
+loop = True
+while loop:
+    print(*result, sep=' ')
+    result[-1] += 1
+    for i in range(M - 1, -1, -1):
+        if i == 0 and result[i] > N:
+            loop = False
+            break
+        if result[i] > N:
+            result[i] = 1
+            result[i - 1] += 1
