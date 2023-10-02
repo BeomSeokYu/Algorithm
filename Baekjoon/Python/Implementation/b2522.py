4c4154ff16bcb21bcdc394731b58f2a8e680ac6a
BeokSeokYu <beomseok.dev@gmail.com>
2023-10-02 19:23:23 +0900

    b2522 - 구현

---
+++ Baekjoon/Python/Implementation/b2522.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N):
+    for j in range(N - i - 1):
+        print(' ', end='')
+    for k in range(i + 1):
+        print('*', end='')
+    print()
+for i in range(N - 1):
+    for j in range(i + 1):
+        print(' ', end='')
+    for k in range(N - i - 1, 0, -1):
+        print('*', end='')
+    print()
