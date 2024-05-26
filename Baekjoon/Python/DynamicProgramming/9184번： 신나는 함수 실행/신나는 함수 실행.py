2d7d3200d6a50112df8eb26211de11073c8e269f
BeomSeokYu <beomseok.dev@gmail.com>
2024-05-26 10:22:21 +0900

    BOJ-EX: 2024. 5. 26. 오전 10:22:20

--- Baekjoon/Python/DynamicProgramming/9184번： 신나는 함수 실행/신나는 함수 실행.py
+++ Baekjoon/Python/DynamicProgramming/9184번： 신나는 함수 실행/신나는 함수 실행.py
@@ -12,26 +12,23 @@
 import sys
 input = sys.stdin.readline
 
-d = dict() # 하향식 재귀의 메모이제이션을 위한 딕셔너리
-
 def w(a, b, c):
-    key = str(a) + str(b) + str(c)
-    if a <= 0 or b <= 0 or c <= 0:
+    if a <= 0 or b<= 0 or c<=0:
         return 1
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
-    if d.get(key) != None:
-        return d.get(key)
-    if a < b and b < c:
-        res = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
-        d[key] = res
-        return res
-    res = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
-    d[key] = res
-    return res
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a<b<c:
+        dp[a][b][c] = w(a, b, c-1)+w(a, b-1, c-1)-w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c)+w(a-1, b-1, c)+w(a-1, b, c-1)-w(a-1, b-1, c-1)
+    return dp[a][b][c]
 
-while True:
+dp = [[[0]*(21) for _ in range(21)] for _ in range(21)] # 0~20까지므로
+
+while 1:
     a, b, c = map(int, input().split())
-    if a == -1 and b == -1 and c == -1:
+    if a==-1 and b==-1 and c==-1:
         break
     print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
